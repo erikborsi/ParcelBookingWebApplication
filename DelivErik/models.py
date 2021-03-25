9c081616898872c889889fbabc4abe54c0b65429
Erik Borsi <erikborsi@users.noreply.github.com>
2021-03-25 17:02:25 +0000

    model created, migrated

--- DelivErik/models.py
+++ DelivErik/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+#from phonenumber_field.modelfields import PhoneNumberField
+# from from django.contrib.gis.measure import D, Distance
 
-# Create your models here.
+class Customer(models.Model):
+    customer_unique_ID = models.IntegerField(blank=False)
+    customer_first_name = models.CharField(max_length=30, blank=False)
+    customer_last_name = models.CharField(max_length=30, blank=False)
+    customer_first_line_address = models.IntegerField(blank=False)
+    customer_second_line_address = models.TextField(blank=False)
+    customer_postcode = models.TextField(blank=False)
+    customer_email_address = models.EmailField(blank=False)
+    customer_phone_number = models.CharField(max_length=30, null=False, blank=False)
+
+class Parcel(models.Model):
+    STATUS_CHOICES = [('R', 'Recieved'), ('C', 'Collected'), ('D', 'Delivered'), ('X', 'Cancelled')]
+    parcel_unique_ID = models.IntegerField(blank=False)
+    parcel_width = models.IntegerField(blank=False)
+    parcel_height = models.IntegerField(blank=False)
+    parcel_length = models.IntegerField(blank=False)
+    parcel_weight = models.IntegerField(blank=False)
+    parcel_description = models.TextField(blank=False)
+    parcel_status_update_time = models.DateField(blank=False)
+    parcel_status = models.CharField(max_length=1, choices=STATUS_CHOICES, blank=False)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+
